96b08717be62af33c40aef8fe2d14c91b7acecbe
ryugaax <96875664+ryugaax@users.noreply.github.com>
2022-08-23 15:35:10 +0800

    loginpage version4
    user's log in detail is saved into a json file now

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 from cgitb import text
 from csv import reader
+import profile
 from sys import dont_write_bytecode
 from tkinter import *
 from turtle import back, window_width
@@ -9,6 +10,8 @@ import signup
 from setuptools import *
 import csv
 import home
+import json
+import resetPassword
 
 class loginForm : 
     def __init__(self,window):
@@ -31,15 +34,18 @@ class loginForm :
         self.lg_frame.place(x=258,y=70)
 
         # ========= heading =========
-        self.heading = Label(self.lg_frame, text='SECURITY CAM' , font=25 , bg='grey') 
+        self.heading = Label(self.lg_frame, text='EZ CAM' , font=25 , bg='grey') 
         self.heading.place(x=8 , y=30 , width=634 , height=30)
 
         # ========= profile picture =========
-        self.profile = Label(self.lg_frame , bg='white') 
-        self.profile.place(x=250 , y=80 , width=150 , height=150)
+        self.pp_frame = Image.open('images\\logo.jpg')
+        profile_photo = ImageTk.PhotoImage(self.pp_frame)
+        self.pp = Label(self.lg_frame , image=profile_photo) 
+        self.pp.image = profile_photo
+        self.pp.place(x=250 , y=80 , width=150 , height=150)
 
         # ========= email =========
-        self.email_label = Label(self.lg_frame , text='email : ' , bg='grey' , font=(30))
+        self.email_label = Label(self.lg_frame , text='Email Address : ' , bg='grey' , font=(30))
         self.email_label.place(x=250 , y= 250 , width=150 , height=20)   
 
         self.email_textbox = Entry(self.lg_frame , font=(25) , bg='grey' , relief='flat' , highlightthickness=0)
@@ -49,10 +55,10 @@ class loginForm :
         self.email_line.place(x=200 , y=300)
 
         # ========= password =========
-        self.password_label = Label(self.lg_frame , text='password : ' , bg='grey' , font=(30))
+        self.password_label = Label(self.lg_frame , text='Password : ' , bg='grey' , font=(30))
         self.password_label.place(x=250 , y= 320 , width=150 , height=20)   
 
-        self.password_textbox = Entry(self.lg_frame , font=(25) , bg='white' , relief='flat' , show='*')
+        self.password_textbox = Entry(self.lg_frame , font=(25) , bg='grey' , relief='flat' , show='*')
         self.password_textbox.place(x=200 , y=350 , width=250 , height=20)
 
         self.password_line = Canvas(self.lg_frame , width= 250 , height=2 , bg='black' , highlightthickness=0 )
@@ -75,12 +81,13 @@ class loginForm :
         self.signup_link = Label(self.lg_frame , text="sign up now" , bg='grey' , font= ('Helvetica 10 underline') , cursor='hand2', fg='white')
         self.signup_link.place(x=350 , y=455 , height=14)
         
-        self.signup_link.bind('<Button-1>' , lambda event:self.loadSignupPage(window))
+        self.signup_link.bind('<Button-1>' , lambda event:self.loadSignupPage())
         
         # ========= forget password =========
         self.forget_pw = Label(self.lg_frame , text="forget password?" , bg='grey' , font= ('Helvetica 10 underline') , cursor='hand2', fg='#99f8d3')
         self.forget_pw.place(x=270 , y=475 , height=14)
 
+        self.forget_pw.bind('<Button-1>' , lambda event:self.loadForgetPage())
     def validateAccount(cls , email_entry , password_entry):
         if os.path.exists('users_account.csv'):
             with open('users_account.csv','r',newline='') as f:
@@ -90,11 +97,13 @@ class loginForm :
                         cls.user_institution = row[0]
                         cls.user_email = row[1]
                         cls.user_password = row[2]
+                        user_dict= {"insti_name" : cls.user_institution , "user_email":cls.user_email , "user_password":cls.user_password}
+                        with open("logging_in.json", "w") as outfile:
+                            json.dump(user_dict, outfile)
                         for widgets in cls.window.winfo_children():
                             widgets.destroy()
                         homePage = home.homePage(cls.window , institution=cls.user_institution)
-                        homePage.page()
-
+                        homePage.loadPage()
     def loginPage(self):
         self.window.mainloop()
     
@@ -102,7 +111,13 @@ class loginForm :
         for widgets in self.window.winfo_children():
             widgets.destroy()
         signUpPage=signup.signupForm(self.window)
-        signUpPage.signupPage(self.window) 
+        signUpPage.signupPage() 
+
+    def loadForgetPage(self):
+        for widgets in self.window.winfo_children():
+            widgets.destroy()
+        forgetPage=resetPassword.resetPage(self.window)
+        forgetPage.loadPage()
 
 def readcsv():
     with open('users_account.csv') as f:
