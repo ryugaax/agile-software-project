4ed3c5ca151d20b454eaf7dc76b7a256d13cb4bf
ryugaax <96875664+ryugaax@users.noreply.github.com>
2022-08-23 15:31:11 +0800

    homepage version 3
    manu bar can navigate the user now

--- home.py
+++ home.py
@@ -6,33 +6,16 @@ from turtle import back, home, width, window_width
 from PIL import ImageTk , Image
 import os
 import signup
-
+import userMenu
 
 class homePage : 
     def __init__(self,window,institution = ""):
         self.window = window
         self.window.geometry('1166x718')
-        menu = Menu(window)
-        window.config(menu=menu , bg='#27285C')
-
-# =============help menu=================
-        helpMenu = Menu(menu , tearoff=0 , activebackground='orange')
-        helpMenu.add_command(label='how to use')
-        helpMenu.add_command(label='about us')
-        menu.add_cascade(label='help' , menu=helpMenu)
-
-# =============setting menu=================      
-        settingMenu = Menu(menu , tearoff=0 , activebackground='orange')
-        settingMenu.add_command(label='account setting')
-        settingMenu.add_command(label='log out')
-        menu.add_cascade(label='setting' , menu=settingMenu)
-
-# =============exit menu=================      
-        exitMenu = Menu(menu , tearoff=0 , activebackground='orange')
-        menu.add_cascade(label='exit' , menu=exitMenu)
+        menu = userMenu.createMenu(window)
 
 # =============logo =================           
-        self.logo = Image.open('images\\login_bg.jpg')
+        self.logo = Image.open('images\\logo_1.jpg')
         photo = ImageTk.PhotoImage(self.logo)
         self.logo_panel = Label(self.window,image=photo)
         self.logo_panel.image = photo
@@ -51,18 +34,20 @@ class homePage :
         self.record_button.bind('<Enter>' , on_enter)
         self.record_button.bind('<Leave>' , on_leave)
 
-    def page(self):
+    def loadPage(self):
         self.window.mainloop()
 
 def startRecording():
         os.system('python site-packages\cam.py')
 
 def on_enter(e):
-   e.widget['background']='violet'
-   e.widget['foreground']= "white"
+        e.widget['background']='violet'
+        e.widget['foreground']= "white"
 
 def on_leave(e):
-   e.widget['background']='#1995CC'
-   e.widget['foreground']= "black"
+        e.widget['background']='#1995CC'
+        e.widget['foreground']= "black"
+
+
 
 #    https://stackoverflow.com/questions/49888623/tkinter-hovering-over-button-color-change
